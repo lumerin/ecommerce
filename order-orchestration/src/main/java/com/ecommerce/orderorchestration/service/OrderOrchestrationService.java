e70e1479121b6f3ea3f35857cce4da265d3c2ce2
Raka Aditya <rakaaditya1304@gmail.com>
2024-01-25 08:49:28 +0700

    Refactor:delete line

--- order-orchestration/src/main/java/com/ecommerce/orderorchestration/service/OrderOrchestrationService.java
+++ order-orchestration/src/main/java/com/ecommerce/orderorchestration/service/OrderOrchestrationService.java
@@ -2,23 +2,18 @@ package com.ecommerce.orderorchestration.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
 
 @Service
 @Slf4j
 public class OrderOrchestrationService {
 
     final JmsTemplate jmsTemplate;
-    final R2dbcEntityTemplate r2dbcEntityTemplate;
 
     @Autowired
-    public OrderOrchestrationService(JmsTemplate jmsTemplate, R2dbcEntityTemplate r2dbcEntityTemplate) {
+    public OrderOrchestrationService(JmsTemplate jmsTemplate) {
         this.jmsTemplate = jmsTemplate;
-        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
     }
 
     public void sendMessage(String queueName, Object object) {
