5e5f935424461bfdb195a1c8542e67811e97c69c
Raka Aditya <rakaaditya1304@gmail.com>
2024-01-25 08:57:24 +0700

    Refactor:add onerror case on sendnotif

--- notif/src/main/java/com/ecommerce/notif/service/NotifService.java
+++ notif/src/main/java/com/ecommerce/notif/service/NotifService.java
@@ -5,28 +5,27 @@ import com.ecommerce.notif.model.Notif;
 import com.ecommerce.notif.model.OrderData;
 import com.ecommerce.notif.model.ResponseNotif;
 import com.ecommerce.notif.model.enums.NotifStatus;
-import com.ecommerce.notif.repository.NotifRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.UUID;
 
 @Service
 @Slf4j
 public class NotifService {
 
+    final JmsTemplate jmsTemplate;
+    final R2dbcEntityTemplate r2dbcEntityTemplate;
+
     @Autowired
-    NotifRepository notifRepository;
-    @Autowired
-    JmsTemplate jmsTemplate;
-    @Autowired
-    R2dbcEntityTemplate r2dbcEntityTemplate;
+    public NotifService(JmsTemplate jmsTemplate, R2dbcEntityTemplate r2dbcEntityTemplate) {
+
+        this.jmsTemplate = jmsTemplate;
+        this.r2dbcEntityTemplate = r2dbcEntityTemplate;
+    }
 
     public void  sendNotif(OrderData data) {
         Notif notifData = new Notif();
@@ -35,23 +34,24 @@ public class NotifService {
         notifData.setCreatedDate(LocalDate.now());
         log.info("notif datanya :{}", notifData);
         r2dbcEntityTemplate.insert(notifData)
-                .doOnSuccess(registered -> {
-                    ResponseNotif responseNotif = new ResponseNotif();
-                    responseNotif.setOrderId(data.getOrderId());
-                    responseNotif.setPriority(data.getPriority());
-                    responseNotif.setData(data.getData());
-                    responseNotif.setNotifStatus("NOTIF_COMPLETED");
-                    jmsTemplate.convertAndSend("queue.notif.status", responseNotif);
-                    log.info("Message sent to orchestrator that notif successfully registered: {}", responseNotif);
-                })
-                .doOnError(error -> {
-                    log.info("notif failed because : {}", error.getMessage());
-                    jmsTemplate.convertAndSend("queue.notif.status", "NOTIF_REJECTED");
-                    log.info("message sent to orchestrator that notif rejected: {}", NotifStatus.NOTIF_REJECTED);
-                }).subscribe();
-    }
-
-    public void rollbackRegister(String notifData) {
-        notifRepository.deleteById(notifData).subscribe();
+            .doOnSuccess(registered -> {
+                ResponseNotif responseNotif = new ResponseNotif();
+                responseNotif.setOrderId(data.getOrderId());
+                responseNotif.setPriority(data.getPriority());
+                responseNotif.setData(data.getData());
+                responseNotif.setNotifStatus("NOTIF_COMPLETED");
+                jmsTemplate.convertAndSend("queue.notif.status", responseNotif);
+                log.info("Message sent to orchestrator that notif successfully registered: {}", responseNotif);
+            })
+            .doOnError(error -> {
+                log.error("notif failed because : {}", error.getMessage());
+                ResponseNotif responseNotif = new ResponseNotif();
+                responseNotif.setOrderId(data.getOrderId());
+                responseNotif.setPriority(data.getPriority());
+                responseNotif.setData(data.getData());
+                responseNotif.setNotifStatus("NOTIF_REJECTED");
+                jmsTemplate.convertAndSend("queue.notif.status", responseNotif);
+                log.info("message sent to orchestrator that notif rejected: {}", NotifStatus.NOTIF_REJECTED);
+            }).subscribe();
     }
 }
